d1c54b457369536598a6d2d543bf7c3711bf9aeb
DeybisMelendez <pixelweb2007@gmail.com>
2019-12-10 13:53:00 -0600

    Dia 7 2019 resuelto

--- 2019/Dia7.lua
+++ 2019/Dia7.lua
@@ -1,7 +1,7 @@
 local inputCode = {
 3,8,1001,8,10,8,105,1,0,0,21,38,63,72,81,106,187,268,349,430,99999,3,9,101,5,9,9,1002,9,3,9,101,3,9,9,4,9,99,3,9,102,3,9,9,101,4,9,9,1002,9,2,9,1001,9,2,9,1002,9,4,9,4,9,99,3,9,1001,9,3,9,4,9,99,3,9,102,5,9,9,4,9,99,3,9,102,4,9,9,1001,9,2,9,1002,9,5,9,1001,9,2,9,102,3,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99
+--3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0
 }
-local output = 0
 
 local sequences = {}
 for a = 0, 4 do
@@ -14,7 +14,7 @@ for a = 0, 4 do
                             for e = 0, 4 do
                                 if not(a==e) and not(b==e) and not(c==e) and not(d==e) then
                                     table.insert(sequences, {a,b,c,d,e})
-                                    print(tostring(a) .. tostring(b) .. tostring(c) .. tostring(d) .. tostring(e))
+                                    --print(tostring(a) .. tostring(b) .. tostring(c) .. tostring(d) .. tostring(e))
                                 end
                             end
                         end
@@ -34,26 +34,23 @@ local function copyTable(t)
     return n
 end
 
-local function intCode(i, iv)
-    local input = copyTable(i)
+local function intCode(table, i)--iv)
+    local input = copyTable(table)
     local pointer = 1 --Tablas en lua comienzan con 1, no 0
     local opCode
+    local nextInput = 1
     while true do
         local code = tostring(input[pointer])
-        --if code:sub(1, 1) ~= 0 then code = code:sub(1, 1) .. "0" .. code:sub(2, code:len()) end
         for _=1, 5-(code:len()) do
             code = "0" .. code
         end
         opCode = tonumber(code:sub(code:len()-1, code:len()))
-        if opCode == 99 then return output, input end
+        if opCode == 99 then return end
         local param1, param2--, param3 parametro 3 tecnicamente no requiere modo inmediato
         if tonumber(code:sub(3,3)) == 1 then param1 = pointer+1 else param1 = input[pointer+1]+1 end
         if opCode ~= 4 and opCode ~= 3 then
             if tonumber(code:sub(2,2)) == 1 then param2 = pointer+2 else param2 = input[pointer+2]+1 end
         end
-        -- if opCode ~= 4 and opCode ~= 3 and opCode ~= 5 and opCode ~= 6 then
-        --     if tonumber(code:sub(1,1)) == 0 then param3 = input[pointer+3]+1 else param3 = pointer+3 end
-        -- end
         if opCode == 1 then
             input[input[pointer+3]+1] = input[param1] + input[param2]
             pointer = pointer + 4
@@ -62,11 +59,12 @@ local function intCode(i, iv)
             pointer = pointer + 4
         -- nuevas instrucciones PARTE 1
         elseif opCode == 3 then
-            input[input[pointer+1]+1] = iv--inputValue
+            input[input[pointer+1]+1] = i[nextInput]--inputValue
+            nextInput = nextInput+1
             pointer = pointer + 2
         elseif opCode == 4 then
-            output = input[input[pointer+1]+1]
-            pointer = pointer + 2
+            return input[input[pointer+1]+1]
+            --pointer = pointer + 2
         -- Nuevas instrucciones PARTE 2
         elseif opCode == 5 then
             if not (input[param1] == 0) then pointer = input[param2]+1 else pointer = pointer + 3 end
@@ -84,12 +82,15 @@ local function intCode(i, iv)
     end
 end
 
-
+--sequences = {{4,3,2,1,0}}
+local result = 0
 for i, v in ipairs(sequences) do
     local inputValue = 0
-    amps = {}
-    for amp = 1, #v do
-        local output, t = intCode(inputCode, v[amp])
-        inputValue = intCode(t,inputValue)
+    for inputInstruction=1, #v do
+        inputValue = intCode(inputCode, {v[inputInstruction], inputValue})
+    end
+    if result < inputValue then
+        result = inputValue
     end
 end
+print("the answer 1 is: " .. result)
