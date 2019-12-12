5be219500e4c2268b9d93457649914100b54a1df
DeybisMelendez <pixelweb2007@gmail.com>
2019-12-12 10:43:58 -0600

    dia 7 2019 parte 1 resuelto

---
+++ 2019/Dia9.lua
@@ -0,0 +1,125 @@
+local inputCode = {
+
+1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1102,3,1,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,904,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1101,0,33,1017,1101,24,0,1014,1101,519,0,1028,1102,34,1,1004,1101,0,31,1007,1101,0,844,1025,1102,0,1,1020,1102,38,1,1003,1102,39,1,1008,1102,849,1,1024,1101,0,22,1001,1102,25,1,1009,1101,1,0,1021,1101,0,407,1022,1101,404,0,1023,1101,0,35,1013,1101,27,0,1011,1101,0,37,1016,1102,1,26,1019,1102,28,1,1015,1101,0,30,1000,1102,1,36,1005,1101,0,29,1002,1101,23,0,1012,1102,1,32,1010,1102,21,1,1006,1101,808,0,1027,1102,20,1,1018,1101,0,514,1029,1102,1,815,1026,109,14,2107,24,-5,63,1005,63,199,4,187,1105,1,203,1001,64,1,64,1002,64,2,64,109,-1,2108,21,-7,63,1005,63,225,4,209,1001,64,1,64,1106,0,225,1002,64,2,64,109,-16,1201,6,0,63,1008,63,35,63,1005,63,249,1001,64,1,64,1106,0,251,4,231,1002,64,2,64,109,9,2102,1,2,63,1008,63,37,63,1005,63,271,1105,1,277,4,257,1001,64,1,64,1002,64,2,64,109,11,1208,-8,23,63,1005,63,293,1105,1,299,4,283,1001,64,1,64,1002,64,2,64,109,8,21107,40,39,-8,1005,1017,319,1001,64,1,64,1106,0,321,4,305,1002,64,2,64,109,-28,2101,0,6,63,1008,63,39,63,1005,63,341,1106,0,347,4,327,1001,64,1,64,1002,64,2,64,109,19,2107,26,-7,63,1005,63,363,1106,0,369,4,353,1001,64,1,64,1002,64,2,64,109,1,1202,-9,1,63,1008,63,39,63,1005,63,395,4,375,1001,64,1,64,1105,1,395,1002,64,2,64,109,9,2105,1,-3,1106,0,413,4,401,1001,64,1,64,1002,64,2,64,109,-13,1207,-4,26,63,1005,63,435,4,419,1001,64,1,64,1105,1,435,1002,64,2,64,109,-1,21101,41,0,7,1008,1019,41,63,1005,63,461,4,441,1001,64,1,64,1105,1,461,1002,64,2,64,109,7,21107,42,43,-2,1005,1017,479,4,467,1105,1,483,1001,64,1,64,1002,64,2,64,109,-6,21108,43,46,0,1005,1013,499,1106,0,505,4,489,1001,64,1,64,1002,64,2,64,109,17,2106,0,-2,4,511,1105,1,523,1001,64,1,64,1002,64,2,64,109,-27,1202,-1,1,63,1008,63,28,63,1005,63,547,1001,64,1,64,1106,0,549,4,529,1002,64,2,64,109,18,1206,-1,567,4,555,1001,64,1,64,1106,0,567,1002,64,2,64,109,-16,21102,44,1,6,1008,1011,43,63,1005,63,587,1106,0,593,4,573,1001,64,1,64,1002,64,2,64,109,8,21102,45,1,-1,1008,1012,45,63,1005,63,619,4,599,1001,64,1,64,1105,1,619,1002,64,2,64,109,7,1205,1,633,4,625,1106,0,637,1001,64,1,64,1002,64,2,64,109,-8,2102,1,-3,63,1008,63,25,63,1005,63,659,4,643,1105,1,663,1001,64,1,64,1002,64,2,64,109,14,1206,-5,679,1001,64,1,64,1105,1,681,4,669,1002,64,2,64,109,-28,2101,0,2,63,1008,63,30,63,1005,63,707,4,687,1001,64,1,64,1106,0,707,1002,64,2,64,109,21,21101,46,0,0,1008,1019,48,63,1005,63,727,1106,0,733,4,713,1001,64,1,64,1002,64,2,64,109,-3,21108,47,47,1,1005,1017,751,4,739,1106,0,755,1001,64,1,64,1002,64,2,64,109,-13,1207,0,37,63,1005,63,771,1105,1,777,4,761,1001,64,1,64,1002,64,2,64,109,7,2108,21,-9,63,1005,63,797,1001,64,1,64,1105,1,799,4,783,1002,64,2,64,109,22,2106,0,-5,1001,64,1,64,1106,0,817,4,805,1002,64,2,64,109,-4,1205,-8,829,1106,0,835,4,823,1001,64,1,64,1002,64,2,64,109,-4,2105,1,0,4,841,1105,1,853,1001,64,1,64,1002,64,2,64,109,-30,1208,6,30,63,1005,63,871,4,859,1105,1,875,1001,64,1,64,1002,64,2,64,109,-2,1201,9,0,63,1008,63,22,63,1005,63,897,4,881,1106,0,901,1001,64,1,64,4,64,99,21101,27,0,1,21102,1,915,0,1106,0,922,21201,1,66266,1,204,1,99,109,3,1207,-2,3,63,1005,63,964,21201,-2,-1,1,21102,942,1,0,1105,1,922,22101,0,1,-1,21201,-2,-3,1,21101,0,957,0,1106,0,922,22201,1,-1,-2,1105,1,968,21202,-2,1,-2,109,-3,2106,0,0
+--104,1125899906842624,99
+--1102,34915192,34915192,7,4,7,99,0
+--109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99
+}
+
+local function copyTable(t)
+    local n = {}
+    for i, v in pairs(t) do
+        n[i] = v
+    end
+    return n
+end
+local function newIntCode(i)
+    local t = {}
+    t.input = copyTable(i)
+    t.nextInput = 1
+    --t.i = i1 --tabla con los inputs
+    t.pointer = 1
+    t.baseRelative = 0
+    --t.i2 = i2
+    function t:intCode(inputValue) --inputValue debe ser una tabla
+        --self.i = self.i or inputValue
+        local opCode
+        while true do
+            local code = tostring(self.input[self.pointer])
+            for _=1, 5-(code:len()) do
+                code = "0" .. code
+            end
+            opCode = tonumber(code:sub(code:len()-1, code:len()))
+            if opCode == 99 then return end --input[input[pointer+1]+1] end
+            local param1, param2, param3-- parametro 3 tecnicamente no requiere modo inmediato
+            -- Modos
+            --TODO agregar modo 2 a los parametros
+            --parametro 1
+            if tonumber(code:sub(3,3)) == 2 then
+                param1 = self.input[self.pointer+1]+1 + self.baseRelative
+            elseif tonumber(code:sub(3,3)) == 1 then
+                param1 = self.pointer+1
+            elseif tonumber(code:sub(3,3)) == 0 then
+                param1 = self.input[self.pointer+1]+1
+            end
+            --parametro 2
+            if opCode ~= 4 and opCode ~= 3 then
+                if tonumber(code:sub(2,2)) == 2 then
+                    param2 = self.input[self.pointer+2]+1 + self.baseRelative
+                elseif tonumber(code:sub(2,2)) == 1 then
+                    param2 = self.pointer+2
+                elseif tonumber(code:sub(2,2)) == 0 then
+                    param2 = self.input[self.pointer+2]+1
+                end
+            end
+            --parametro 3
+            if tonumber(code:sub(1,1)) == 2 then
+                param3 = self.input[self.pointer+3]+1 + self.baseRelative
+            end
+            if tonumber(code:sub(1,1)) == 1 then
+                param3 = self.pointer+3
+            elseif tonumber(code:sub(1,1)) == 0 then
+                param3 = self.input[self.pointer+3]+1
+            end
+            -- OP CODES
+            --local continue = false
+            if opCode == 1 then
+                self.input[param3] = self.input[param1] + self.input[param2]
+                self.pointer = self.pointer + 4
+            elseif opCode == 2 then
+                self.input[param3] = self.input[param1] * self.input[param2]
+                self.pointer = self.pointer + 4
+            -- nuevas instrucciones PARTE 1
+            elseif opCode == 3 then
+                self.input[param3] = inputValue[self.nextInput] or inputValue[1]
+                self.nextInput = self.nextInput+1
+                self.pointer = self.pointer + 2
+            elseif opCode == 4 then
+                -- self.input = input
+                -- self.nextInput = nextInput
+                -- self.pointer = pointer + 2
+                return self.input[param1]--input[input[pointer+1]+1]
+            -- Nuevas instrucciones PARTE 2
+            elseif opCode == 5 then
+                if not (self.input[param1] == 0) then
+                    self.pointer = self.input[param2]+1
+                else
+                    self.pointer = self.pointer + 3
+                end
+            elseif opCode == 6 then
+                if self.input[param1] == 0 then
+                    self.pointer = self.input[param2]+1
+                else
+                    self.pointer = self.pointer + 3
+                end
+            elseif opCode == 7 then
+                if self.input[param1] < self.input[param2] then
+                    self.input[param3] = 1
+                else
+                    self.input[param3] = 0
+                end
+                self.pointer = self.pointer + 4
+            elseif opCode == 8 then
+                if self.input[param1] == self.input[param2] then
+                    self.input[param3] = 1
+                else
+                    self.input[param3] = 0
+                end
+                self.pointer = self.pointer + 4
+            elseif opCode == 9 then
+                self.baseRelative = self.baseRelative + self.input[param1]
+                --if self.baseRelative < 0 then self.baseRelative = 0 end
+                self.pointer = self.pointer + 2
+            else
+                error("opCode desconocido: " .. opCode)
+            end
+        end
+    end
+    return t
+end
+
+local intCode = newIntCode(inputCode)
+-- for _, v in ipairs(intCode.input) do
+--     print(v)
+-- end
+print("answer 1 is", intCode:intCode({1}))
