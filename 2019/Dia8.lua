f4d73fa519a5039a372c4c186bdbdb7842e24610
DeybisMelendez <pixelweb2007@gmail.com>
2019-12-11 20:45:27 -0600

    dia 7 2019 resuelto

--- 2019/Dia8.lua
+++ 2019/Dia8.lua
@@ -16,25 +16,59 @@ local function get1by2digits(str)
     return one * two
 end
 
-local function result1(str, digit, width, height)
-    local layers = {}
-    local result = 0
-    for layer=0, str:len()/(width*height)-1 do
-        table.insert(layers, str:sub((width*height*layer)+1, width*height*(layer+1)))
+local function getLayers(str, width, height)
+    local t = {}
+    for layer=0, str:len() / (width * height) - 1 do
+        table.insert(t, str:sub((width * height * layer) + 1, width * height * (layer + 1)))
     end
+    return t
+end
+
+local function result1(str, digit, width, height)
+    local layers = getLayers(str, width, height)
+    local result = 0
+
     local totalDigits
-    for i, layer in ipairs(layers) do
+    for _, layer in ipairs(layers) do
         local layerDigits = 0
         for char=1, layer:len() do
             if layer:sub(char, char) == digit then
                 layerDigits = layerDigits + 1
             end
         end
-        if totalDigits==nil or totalDigits >= layerDigits then
+        if totalDigits == nil or totalDigits >= layerDigits then
             totalDigits = layerDigits
             result = get1by2digits(layer)
         end
     end
+    print(#layers)
     return result
 end
+
+local function result2(str, width, height)
+    local layers = getLayers(str, width, height)
+    local result = {}
+    local newStr = ""
+    for char=1, layers[1]:len() do
+
+        for _, layer in ipairs(layers) do
+            if not result[char] then
+                result[char] = layer:sub(char,char)
+            elseif tonumber(result[char]) == 2 then
+                result[char] = layer:sub(char,char)
+            end
+        end
+    end
+    local line = 25
+    for index, char in ipairs(result) do
+        if index > line then
+            newStr = newStr .. "\n"
+            line = line + 25
+        end
+        newStr = newStr .. char
+    end
+    return newStr
+end
+
 print("the answer 1 is:",result1(text,"0",25,6))
+print("the answer 2 is: \n" .. result2(text, 25, 6))
