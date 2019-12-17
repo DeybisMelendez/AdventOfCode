579144a836d5c93f173eeeeccefdcc43ebee4cd0
DeybisMelendez <pixelweb2007@gmail.com>
2019-12-17 15:54:32 -0600

    dia 12 parte 1 resuelto

--- 2019/Dia12.lua
+++ 2019/Dia12.lua
@@ -1,68 +1,66 @@
-local function newPlanet(x, y, z)
-	return {pos = {x = x, y = y, z = z}, speed = {x = 0, y = 0, z = 0}}
+local input = {
+	{pos = {x=-2, y=9, z=-5}, speed = {x=0,y=0,z=0}},
+	{pos = {x=16, y=19, z=9}, speed = {x=0,y=0,z=0}},
+	{pos ={x=0, y=3, z=6}, speed = {x=0,y=0,z=0}},
+	{pos ={x=11, y=0, z=11}, speed = {x=0,y=0,z=0}}
+--	{pos = {x=-1, y=0, z=2}, speed = {x=0,y=0,z=0}},
+--	{pos = {x=2, y=-10, z=-7}, speed = {x=0,y=0,z=0}},
+--	{pos ={x=4, y=-8, z=8}, speed = {x=0,y=0,z=0}},
+--	{pos ={x=3, y=5, z=-1}, speed = {x=0,y=0,z=0}}
+}
+function deepcopy(orig)
+    local orig_type = type(orig)
+    local copy
+    if orig_type == 'table' then
+        copy = {}
+        for orig_key, orig_value in next, orig, nil do
+            copy[deepcopy(orig_key)] = deepcopy(orig_value)
+        end
+        setmetatable(copy, deepcopy(getmetatable(orig)))
+    else -- number, string, boolean, etc
+        copy = orig
+    end
+    return copy
 end
-local planets = {}
-planets[1] = newPlanet(-1,0,2)--(-2,9,-5) +1 +1 +1
-planets[2] = newPlanet(2,-10,-7)--(16,19,9)
-planets[3] = newPlanet(4,-8,8)--(0,3,6)
-planets[4] = newPlanet(3,5,-1)--(11,0,11)
-
-local function calcGrav(value1, value2)
-	local x1, y1, z1 = 0, 0, 0
-	if value2.pos.x > value1.pos.x then x1 = 1 elseif value2.pos.x < value1.pos.x then x1 = -1 end
-	if value2.pos.y > value1.pos.y then y1 = 1 elseif value2.pos.y < value1.pos.y then y1 = -1 end
-	if value2.pos.z > value1.pos.z then z1 = 1 elseif value2.pos.z < value1.pos.z then z1 = -1 end
-	print(x1,y1,z1)
-	return x1, y1, z1
+local function getNewSpeed(a, b)
+	local x, y, z = 0, 0, 0 -- gravedad
+	if b.pos.x > a.pos.x then x = 1 elseif b.pos.x < a.pos.x then x = -1 end
+	if b.pos.y > a.pos.y then y = 1 elseif b.pos.y < a.pos.y then y = -1 end
+	if b.pos.z > a.pos.z then z = 1 elseif b.pos.z < a.pos.z then z = -1 end
+	return {x = x + a.speed.x, y = y + a.speed.y, z = z + a.speed.z}
 end
 
-for step=1, 10 do
-	
-	--print(value1.pos.x, value1.pos.y, value1.pos.z, "speed ", planets[p1].speed.x, planets[p1].speed.y, planets[p1].speed.z)
-	-- Aplicamos gravedad
-	local x, y, z = calcGrav(planets[1],planets[2])
-	planets[1].pos.x, planets[1].pos.y, planets[1].pos.z = planets[1].pos.x + x, planets[1].pos.y + y, planets[1].pos.z + z
-	x, y, z = calcGrav(planets[1],planets[3])
-	planets[1].pos.x, planets[1].pos.y, planets[1].pos.z = planets[1].pos.x + x, planets[1].pos.y + y, planets[1].pos.z + z
-	x, y, z = calcGrav(planets[1],planets[4])
-	planets[1].pos.x, planets[1].pos.y, planets[1].pos.z = planets[1].pos.x + x, planets[1].pos.y + y, planets[1].pos.z + z
-	
-	x, y, z = calcGrav(planets[2],planets[1])
-	planets[2].pos.x, planets[2].pos.y, planets[2].pos.z = planets[2].pos.x + x, planets[2].pos.y + y, planets[2].pos.z + z
-	x, y, z = calcGrav(planets[2],planets[3])
-	planets[2].pos.x, planets[2].pos.y, planets[2].pos.z = planets[2].pos.x + x, planets[2].pos.y + y, planets[2].pos.z + z
-	x, y, z = calcGrav(planets[2],planets[4])
-	planets[2].pos.x, planets[2].pos.y, planets[2].pos.z = planets[2].pos.x + x, planets[2].pos.y + y, planets[2].pos.z + z
-	
-	x, y, z = calcGrav(planets[3],planets[1])
-	planets[3].pos.x, planets[3].pos.y, planets[3].pos.z = planets[3].pos.x + x, planets[3].pos.y + y, planets[3].pos.z + z
-	x, y, z = calcGrav(planets[3],planets[2])
-	planets[3].pos.x, planets[3].pos.y, planets[3].pos.z = planets[3].pos.x + x, planets[3].pos.y + y, planets[3].pos.z + z
-	x, y, z = calcGrav(planets[3],planets[4])
-	planets[3].pos.x, planets[3].pos.y, planets[3].pos.z = planets[3].pos.x + x, planets[3].pos.y + y, planets[3].pos.z + z
-	
-	
-	x, y, z = calcGrav(planets[4],planets[1])
-	planets[4].pos.x, planets[4].pos.y, planets[4].pos.z = planets[4].pos.x + x, planets[4].pos.y + y, planets[4].pos.z + z
-	x, y, z = calcGrav(planets[4],planets[2])
-	planets[4].pos.x, planets[4].pos.y, planets[4].pos.z = planets[4].pos.x + x, planets[4].pos.y + y, planets[4].pos.z + z
-	x, y, z = calcGrav(planets[4],planets[3])
-	planets[4].pos.x, planets[4].pos.y, planets[4].pos.z = planets[4].pos.x + x, planets[4].pos.y + y, planets[4].pos.z + z
-	--Aplicamos velocidad
-	--print(step)
-	for p1, value in ipairs(planets) do
-		planets[p1].pos.x, planets[p1].pos.y, planets[p1].pos.z = planets[p1].pos.x + planets[p1].speed.x, planets[p1].pos.y + planets[p1].speed.y, planets[p1].pos.z + planets[p1].speed.z
+local function getNewPos(a)
+	return {x = a.pos.x + a.speed.x, y = a.pos.y + a.speed.y, z = a.pos.z + a.speed.z}
+end
+
+local function getEnergy(p)
+	local abs = math.abs
+	local potential = abs(p.pos.x) + abs(p.pos.y) + abs(p.pos.z)
+	local kinetic = abs(p.speed.x) + abs(p.speed.y) + abs(p.speed.z)
+	return potential * kinetic
+end
+
+local function answer1()
+	local planets = deepcopy(input)
+	local totalEnergy = 0
+	for step=1, 1000 do
+		for i1, v1 in ipairs(planets) do
+			-- aplicamos la gravedad
+			for i2, v2 in ipairs(planets) do
+				v1.speed = getNewSpeed(v1, v2)
+			end
+		end
+		for _, v in ipairs(planets) do
+			-- aplicamos velocidad
+			v.pos = getNewPos(v)
+			--print(step, v1.pos.x, v1.pos.y, v1.pos.z, v1.speed.x, v1.speed.y, v1.speed.z)
+		end
 	end
-
-end
-local potentialEnergy = 0
-local KineticEnergy = 0
-local result = 0
-for planet, value in ipairs(planets) do
-	local xp, yp, zp = math.abs(value.pos.x), math.abs(value.pos.y), math.abs(value.pos.z)
-	local xk, yk, zk = math.abs(value.speed.x), math.abs(value.speed.y), math.abs(value.speed.z)
-	result = result + ((xp+yp+zp)*(xk+yk+zk))
-	print(xp, yp, zp, xk, yk, zk)
+	for _, v in ipairs(planets) do
+		totalEnergy = totalEnergy + getEnergy(v)
+	end
+	return totalEnergy
 end
 
-print("answer 1 is", result)
+print("answer 1 is", answer1())
