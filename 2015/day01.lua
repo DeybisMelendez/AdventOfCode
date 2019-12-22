ba787f4f92ce00263b98eff59ebe9099c915e5fb
DeybisMelendez <pixelweb2007@gmail.com>
2019-12-21 21:50:30 -0600

    dia 1 2015 resuelto

---
+++ 2015/day01.lua
@@ -0,0 +1,32 @@
+local input = require "day01input"
+
+local function answer1()
+	local result = 0
+	for char in input:gmatch(".") do
+		if char == "(" then
+			result = result + 1
+		elseif char == ")" then
+			result = result - 1
+		end
+	end
+	return result
+end
+
+local function answer2()
+	local floor = 0
+	local result = 0
+	for char in input:gmatch(".") do
+		result = result + 1
+		if char == "(" then
+			floor = floor + 1
+		elseif char == ")" then
+			floor = floor - 1
+		end
+		if floor == -1 then
+			return result
+		end
+	end
+end
+
+print("answer 1 is", answer1())
+print("answer 2 is", answer2())
