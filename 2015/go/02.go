02a9fef34c0474621298b7ad3f5d97de66d81119
DeybisMelendez <pixelweb2007@gmail.com>
2020-11-24 15:37:23 -0600

    dia 2 2015 completado con go

--- 2015/go/02.go
+++ 2015/go/02.go
@@ -2,14 +2,46 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math"
 	"regexp"
+	"strconv"
 )
 
-func getParams(input string) [][]string {
-	regex := regexp.MustCompile("\\d*x\\d*x\\d*")
-	return regex.FindAllStringSubmatch(input, -1)
+func getParams(params string) [][]string {
+	regex := regexp.MustCompile("(\\d*)x(\\d*)x(\\d*)")
+	return regex.FindAllStringSubmatch(params, -1)
+}
+
+func answer1(input string) int {
+	params := getParams(input)
+	total := 0.0
+	for _, gift := range params {
+		l, _ := strconv.ParseFloat(gift[1], 64)
+		w, _ := strconv.ParseFloat(gift[2], 64)
+		h, _ := strconv.ParseFloat(gift[3], 64)
+		total += 2*l*w + 2*w*h + 2*h*l + math.Min(l*w, math.Min(w*h, h*l))
+	}
+	return int(total)
+}
+
+func answer2(input string) int {
+	params := getParams(input)
+	total := 0.0
+	for _, gift := range params {
+		l, _ := strconv.ParseFloat(gift[1], 64)
+		w, _ := strconv.ParseFloat(gift[2], 64)
+		h, _ := strconv.ParseFloat(gift[3], 64)
+		sum := l + w + h
+		max := math.Max(l, math.Max(w, h))
+		total += 2*(sum-max) + l*w*h
+	}
+	return int(total)
 }
 
 func main() {
-	fmt.Println(getParams(ioutil.readFile("01input.txt")))
+	input, _ := ioutil.ReadFile("02input.txt")
+	text := string(input)
+	fmt.Println(answer1(text))
+	fmt.Println(answer2(text))
 }
