98866b16efb0ea8e54a856c51842bff12d6899d7
DeybisMelendez <pixelweb2007@gmail.com>
2020-12-04 22:08:33 -0600

    dia 4 2020 primera parte mas avance segunda parte

---
+++ 2020/go/04/04.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func getInput(inputStr string) []string {
+	return strings.Split(inputStr, "\n\n")
+}
+
+func getPassportParams(str string) []string {
+	regex := regexp.MustCompile("([\\w,\\d,:,#]+)")
+	return regex.FindAllString(str, -1)
+}
+func answer1(inputStr string) int {
+	input := getInput(inputStr)
+	valids := 0
+	for _, v := range input {
+		passport := getPassportParams(v)
+		hasCID := false
+		for _, e := range passport {
+			if e[:3] == "cid" {
+				hasCID = true
+				break
+			}
+		}
+		if hasCID {
+			if len(passport) == 8 {
+				valids++
+			}
+		} else if len(passport) == 7 {
+			valids++
+		}
+	}
+	return valids
+}
+
+func answer2(inputStr string) int {
+	input := getInput(inputStr)
+	valids := 0
+	for _, v := range input {
+		passport := getPassportParams(v)
+		hasCID := false
+		isValid := true
+		for _, e := range passport {
+			key, value := e[:3], e[4:]
+			switch key {
+			case "byr":
+				num, _ := strconv.Atoi(value)
+				if !(num >= 1920 && num <= 2002) && len(value) == 4 {
+					isValid = false
+					break
+				}
+			case "iyr":
+				num, _ := strconv.Atoi(value)
+				if !(num >= 2010 && num <= 2020) && len(value) == 4 {
+					isValid = false
+					break
+				}
+			case "eyr":
+				num, _ := strconv.Atoi(value)
+				if !(num >= 2020 && num <= 2030) && len(value) == 4 {
+					isValid = false
+					break
+				}
+			case "hgt":
+				unit := e[len(e)-2:]
+				if unit == "cm" || unit == "in" {
+					num, err := strconv.Atoi(e[4 : len(e)-2])
+					if err != nil {
+						isValid = false
+						break
+					} else {
+						if unit == "cm" && !(num >= 150 && num <= 193) {
+							isValid = false
+							break
+						} else if unit == "in" && !(num >= 59 && num <= 76) {
+							isValid = false
+							break
+						}
+					}
+				} else {
+					isValid = false
+					break
+				}
+			case "hcl":
+				regex := regexp.MustCompile("#[0-9,a-f]+")
+				hex := regex.FindString(value)
+				//fmt.Println(hex)
+				if hex == "" || len(hex) != 7 {
+					isValid = false
+					break
+				}
+			case "ecl":
+				switch value {
+				case "amb", "blu", "brn", "gry", "grn", "hzl", "oth":
+				default:
+					isValid = false
+					break
+				}
+			case "pid":
+				_, err := strconv.Atoi(value)
+				if len(value) != 9 && err != nil {
+					isValid = false
+					break
+				}
+			case "cid":
+				hasCID = true
+			}
+
+		}
+		if isValid {
+			if hasCID {
+				if len(passport) == 8 {
+					valids++
+				}
+			} else if len(passport) == 7 {
+				valids++
+			}
+		}
+	}
+	return valids
+}
+
+func main() {
+	input, _ := ioutil.ReadFile("input.txt")
+	fmt.Println(answer1(string(input)))
+	fmt.Println(answer2(string(input)))
+}
