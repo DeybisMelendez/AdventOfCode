9932597efe0258cd66c98f2dcb54169f2edb9ead
DeybisMelendez <pixelweb2007@gmail.com>
2020-12-10 12:19:14 -0600

    dia 9 2020 completado

--- 2020/go/09/09.go
+++ 2020/go/09/09.go
@@ -11,6 +11,30 @@ func getInput(inputStr string) []string {
 	return strings.Split(inputStr, "\n")
 }
 
+func min(s []string) int {
+	result, _ := strconv.Atoi(s[0])
+	s = s[1:]
+	for _, v := range s {
+		val, _ := strconv.Atoi(v)
+		if result > val {
+			result = val
+		}
+	}
+	return result
+}
+
+func max(s []string) int {
+	result, _ := strconv.Atoi(s[0])
+	s = s[1:]
+	for _, v := range s {
+		val, _ := strconv.Atoi(v)
+		if result < val {
+			result = val
+		}
+	}
+	return result
+}
+
 func answer1(inputStr string, index int) int {
 	input := getInput(inputStr)
 	for len(input) >= index {
@@ -41,8 +65,34 @@ func answer1(inputStr string, index int) int {
 	return -1
 }
 
+func answer2(inputStr string, index int) int {
+	target := answer1(inputStr, index)
+	input := getInput(inputStr)
+	i := 1
+	total := 0
+	sums := []string{}
+	for total != target {
+		sums = input[0:i]
+		for _, v := range sums {
+			val, _ := strconv.Atoi(v)
+			total += val
+		}
+		if total > target {
+			input = input[1:]
+			i = 1
+			total = 0
+		} else if total < target {
+			i++
+			total = 0
+		}
+	}
+	//fmt.Println(sums, min(sums), max(sums))
+	return min(sums) + max(sums)
+}
+
 func main() {
 	input, _ := ioutil.ReadFile("input.txt")
-	fmt.Println(answer1(string(input), 25))
-	//fmt.Println(answer2(string(input)))
+	preamble := 25
+	fmt.Println(answer1(string(input), preamble))
+	fmt.Println(answer2(string(input), preamble))
 }
