dea6f8af26e50e122d05abde3bf49dd2c729fc2e
deybis <damv2007@gmail.com>
2023-12-07 16:06:08 -0600

    parte 2 dia 7 añadido, cambiando la forma de solucionar de la parte 1 por una mas clara

--- 2023/lua/07.lua
+++ 2023/lua/07.lua
@@ -14,91 +14,107 @@ local highCard = 10000000000
 local tiebreaker = {100000000, -- Desempate, Cada carta se multiplica por este valor
 1000000, 10000, 100, 1}
 local cardValue = {
-    ["A"] = 13,
-    ["K"] = 12,
-    ["Q"] = 11,
-    ["J"] = 10,
-    ["T"] = 9,
-    ["9"] = 8,
-    ["8"] = 7,
-    ["7"] = 6,
-    ["6"] = 5,
-    ["5"] = 4,
-    ["4"] = 3,
-    ["3"] = 2,
-    ["2"] = 1
+    [1] = { -- Valores de cartas en parte 1
+        ["A"] = 13,
+        ["K"] = 12,
+        ["Q"] = 11,
+        ["J"] = 10,
+        ["T"] = 9,
+        ["9"] = 8,
+        ["8"] = 7,
+        ["7"] = 6,
+        ["6"] = 5,
+        ["5"] = 4,
+        ["4"] = 3,
+        ["3"] = 2,
+        ["2"] = 1
+    },
+    [2] = { -- Valores de cartas en parte 2
+        ["A"] = 13,
+        ["K"] = 12,
+        ["Q"] = 11,
+        ["T"] = 10,
+        ["9"] = 9,
+        ["8"] = 8,
+        ["7"] = 7,
+        ["6"] = 6,
+        ["5"] = 5,
+        ["4"] = 4,
+        ["3"] = 3,
+        ["2"] = 2,
+        ["J"] = 1
+    }
 }
 
-local function getStrength(hand)
+local function getStrength(hand, part)
     local strength = 0
     local reps = {}
-    local isFiveOfAKind = false
-    local isPoker = false
-    local isFullHouse = false
-    local isThreeOfAKind = false
-    local isTwoPair = false
-    local isOnePair = false
     local highCardType = "2"
+    local jokers = 0
+    if part == 2 then
+        highCardType = "J"
+    end
     for i = 1, #hand do
         local char = hand:sub(i, i)
+        if char == "J" and part == 2 then
+            jokers = jokers + 1
+        end
         -- Desempate: Sumamos el valor de cada carta segun el orden
-        strength = strength + cardValue[char] * tiebreaker[i]
+        strength = strength + cardValue[part][char] * tiebreaker[i]
         -- Registramos la carta mas alta highCard
-        if cardValue[char] > cardValue[highCardType] then
+        if cardValue[part][char] > cardValue[part][highCardType] then
             highCardType = char
         end
         -- Contamos las repeticiones para determinar el tipo de mano
-        if reps[char] == nil then
-            reps[char] = 1
-        else
-            reps[char] = reps[char] + 1
-        end
-    end
-    for char, rep in pairs(reps) do
-        -- Evaluamos el tipo de mano
-        if rep == 5 then
-            -- Five Of A Kind
-            isFiveOfAKind = true
-        elseif rep == 4 then
-            -- Poker
-            isPoker = true
-        elseif rep == 3 then
-            -- Three Of A Kind
-            isThreeOfAKind = true
-        elseif rep == 2 then
-            -- Pair
-            if not isOnePair then
-                isOnePair = true
-            else -- Two Pair
-                isTwoPair = true
+        if part == 1 then
+            if reps[char] == nil then
+                reps[char] = 1
+            else
+                reps[char] = reps[char] + 1
+            end
+        elseif part == 2 and char ~= "J" then
+            if reps[char] == nil then
+                reps[char] = 1
+            else
+                reps[char] = reps[char] + 1
             end
         end
     end
-
-    -- Verificamos si es full house
-    if isThreeOfAKind and isOnePair then
-        isFullHouse = true
+    local sorted = {} -- ordeno las repeticiones de mayor a menor para identificar la mano facilmente
+    for _, rep in pairs(reps) do
+        table.insert(sorted, rep)
     end
-    -- Puntuamos en orden de tipo
-    if isFiveOfAKind then
+    table.sort(sorted, function(a, b)
+        return a > b
+    end)
+    -- esto puede suceder en la segunda parte ya que no tomo en cuenta los jokers
+    if sorted[1] == nil then
+        sorted[1] = 0
+    end
+    if sorted[2] == nil then
+        sorted[2] = 0
+    end
+    -- identifico el tipo de mano y doy puntuación
+    if sorted[1] + jokers == 5 then
         strength = strength + fiveOfAKind
-    elseif isPoker then
+    elseif sorted[1] + jokers == 4 then
         strength = strength + poker
-    elseif isFullHouse then
+    elseif sorted[1] + jokers == 3 and sorted[2] == 2 then
         strength = strength + fullHouse
-    elseif isThreeOfAKind then
+    elseif sorted[1] + jokers == 3 then
         strength = strength + threOfAKind
-    elseif isTwoPair then
+    elseif sorted[1] == 2 and sorted[2] + jokers == 2 then
         strength = strength + twoPair
-    elseif isOnePair then
+    elseif sorted[1] + jokers == 2 then
         strength = strength + pair
     else
         strength = strength + highCard
     end
+
     return strength
 end
 
-local function getInput()
+local function getInput(part)
     local input = readFile("07.input")
     local hands = {}
     local lines = splitString(input, lineDelimiter)
@@ -107,14 +123,13 @@ local function getInput()
         table.insert(hands, {
             hand = line[1],
             bid = line[2],
-            strength = getStrength(line[1]) -- En base a la puntuación se puede ordenar, el rango sería el index
+            strength = getStrength(line[1], part) -- En base a la puntuación se puede ordenar, el rango sería el index
         })
     end
     return hands
 end
 
-local function answer1()
-    local hands = getInput()
+local function getTotal(hands)
     local bestI = 1 -- en Lua se itera desde 1, no 0
     local minStrength = 99999999999999999
     local rank = 1
@@ -136,4 +151,16 @@ local function answer1()
     return total
 end
 
+local function answer1()
+    local hands = getInput(1)
+
+    return getTotal(hands)
+end
+
+local function answer2()
+    local hands = getInput(2)
+    return getTotal(hands)
+end
+
 print("Parte 1:", answer1())
+print("Parte 2:", answer2())
