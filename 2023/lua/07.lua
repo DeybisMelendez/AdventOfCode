a0dd484e1739da54c4514377fd81926d67a6c29f
deybis <damv2007@gmail.com>
2023-12-07 11:57:04 -0600

    simplificacion de la respuesta 1

--- 2023/lua/07.lua
+++ 2023/lua/07.lua
@@ -1,23 +1,18 @@
 require "utils"
 -- La idea es utilizar los digitos para separar las puntuaciones y dar
 -- una puntuación única a cada mano, incluyendo el desempate.
--- los primeros 2 ceros mas altos puntuan el tipo de carta que forma parte el tipo de mano 1-13.
--- Los siguientes 4 ceros puntuan la diferencia en fullHouse y dos pares,primeros 2 para el mas alto,
--- y los otros 2 para el segundo mas alto.
--- Esto debido a que hay combinaciones de fullHouse y dos pares que deben una puntuación única.
--- luego cada 2 ceros en adelante puntuan los desempates por cartas
-local fiveOfAKind = 70000000000000000
-local poker = 60000000000000000
-local fullHouse = 50000000000000000 -- 156 combinaciones posibles de fullHouse
-local threOfAKind = 40000000000000000
-local twoPair = 30000000000000000 -- 156 combinaciones posibles de dos pares
-local pair = 20000000000000000
-local highCard = 10000000000000000
-local typeCardValue = 100000000000000
+-- El primer dígito indica el tipo de mano, y a partir de ahi, cada dos digitos
+-- es una puntuación 1-13 indicando el desempate de cada carta
+
+local fiveOfAKind = 70000000000
+local poker = 60000000000
+local fullHouse = 50000000000 -- 156 combinaciones posibles de fullHouse
+local threOfAKind = 40000000000
+local twoPair = 30000000000 -- 156 combinaciones posibles de dos pares
+local pair = 20000000000
+local highCard = 10000000000
 local tiebreaker = {100000000, -- Desempate, Cada carta se multiplica por este valor
 1000000, 10000, 100, 1}
-local firstHighValue = 1000000000000 -- para dar valor en fullhouse y dos pares
-local secondHighValue = 10000000000 -- para dar valor en fullhouse y dos pares
 local cardValue = {
     ["A"] = 13,
     ["K"] = 12,
@@ -44,8 +39,6 @@ local function getStrength(hand)
     local isTwoPair = false
     local isOnePair = false
     local highCardType = "2"
-    local typeCard1 = ""
-    local typeCard2 = ""
     for i = 1, #hand do
         local char = hand:sub(i, i)
         -- Desempate: Sumamos el valor de cada carta segun el orden
@@ -66,23 +59,18 @@ local function getStrength(hand)
         if rep == 5 then
             -- Five Of A Kind
             isFiveOfAKind = true
-            typeCard1 = char
         elseif rep == 4 then
             -- Poker
             isPoker = true
-            typeCard1 = char
         elseif rep == 3 then
             -- Three Of A Kind
             isThreeOfAKind = true
-            typeCard1 = char
         elseif rep == 2 then
             -- Pair
             if not isOnePair then
                 isOnePair = true
-                typeCard2 = char -- necesario para no borrar la carta en caso de fullhouse
             else -- Two Pair
                 isTwoPair = true
-                typeCard1 = char
             end
         end
     end
@@ -93,25 +81,19 @@ local function getStrength(hand)
     end
     -- Puntuamos en orden de tipo
     if isFiveOfAKind then
-        strength = strength + fiveOfAKind -- + (typeCardValue * cardValue[typeCard1])
+        strength = strength + fiveOfAKind
     elseif isPoker then
-        strength = strength + poker -- + (typeCardValue * cardValue[typeCard1])
+        strength = strength + poker
     elseif isFullHouse then
-        strength = strength + fullHouse -- + (firstHighValue * cardValue[typeCard1]) +
-        -- (secondHighValue * cardValue[typeCard2])
+        strength = strength + fullHouse
     elseif isThreeOfAKind then
-        strength = strength + threOfAKind -- + (typeCardValue * cardValue[typeCard1])
+        strength = strength + threOfAKind
     elseif isTwoPair then
         strength = strength + twoPair
-        --[[if cardValue[typeCard1] > cardValue[typeCard2] then
-            strength = strength -- + (firstHighValue * cardValue[typeCard1]) + (secondHighValue * cardValue[typeCard2])
-        else
-            strength = strength -- + (secondHighValue * cardValue[typeCard1]) + (firstHighValue * cardValue[typeCard2])
-        end--]]
     elseif isOnePair then
-        strength = strength + pair -- + (typeCardValue * cardValue[typeCard2])
+        strength = strength + pair
     else
-        strength = strength + highCard -- + (typeCardValue * cardValue[highCardType])
+        strength = strength + highCard
     end
     return strength
 end
@@ -145,8 +127,8 @@ local function answer1()
                 bestI = i
             end
         end
-        print("rank: " .. rank, "hand: " .. hands[bestI].hand, "bid: " .. hands[bestI].bid,
-            "winnings: " .. hands[bestI].bid * rank)
+        -- print("rank: " .. rank, "hand: " .. hands[bestI].hand, "bid: " .. hands[bestI].bid,
+        --    "winnings: " .. hands[bestI].bid * rank)
         total = total + (hands[bestI].bid * rank)
         rank = rank + 1
         table.remove(hands, bestI)
