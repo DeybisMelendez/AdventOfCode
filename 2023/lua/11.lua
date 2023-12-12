13ee5406d8985d4b625624d8dce29cb8c040c69c
deybis <damv2007@gmail.com>
2023-12-12 07:34:06 -0600

    no era necesario buscar el path, concalcular cualquier distancia se resuelve mas rapido

--- 2023/lua/11.lua
+++ 2023/lua/11.lua
@@ -3,28 +3,33 @@ require "utils"
 local function getInput()
     local input = readFile("11.input")
     local lines = splitString(input, lineDelimiter)
-    local galaxy = {}
+    local map = {}
     local containsGalaxy = false
     local rankExpanded = {}
     local fileExpanded = {}
+    local galaxies = {}
+
     for i, line in ipairs(lines) do
         containsGalaxy = false
         line = splitString(line, charDelimiter)
-        for _, char in ipairs(line) do
+        for j, char in ipairs(line) do
             if char == "#" then
                 containsGalaxy = true
-                break
+                table.insert(galaxies, {
+                    x = j,
+                    y = i
+                })
             end
         end
-        table.insert(galaxy, line)
+        table.insert(map, line)
         if not containsGalaxy then
             table.insert(rankExpanded, i)
         end
     end
-    for file = #galaxy[1], 1, -1 do
+    for file = 1, #map[1] do
         containsGalaxy = false
-        for rank = 1, #galaxy do
-            if galaxy[rank][file] == "#" then
+        for rank = 1, #map do
+            if map[rank][file] == "#" then
                 containsGalaxy = true
                 break
             end
@@ -34,92 +39,22 @@ local function getInput()
         end
     end
 
-    galaxy.rankExpanded = rankExpanded
-    galaxy.fileExpanded = fileExpanded
-    return galaxy
-end
-
-local function isVisited(visited, pos)
-    for _, value in pairs(visited) do
-        if value.x == pos.x and value.y == pos.y then
-            return true
-        end
-    end
-    return false
-end
-
-local function getPath(from, to)
-    local path = {}
-    local visited = {}
-    local x, y = from.x, from.y
-    local toX, toY = to.x, to.y
-    local newPath = false
-    while true do
-        newPath = true
-        if toX > x and not isVisited(visited, {
-            x = x + 1,
-            y = y
-        }) then
-            x = x + 1
-        elseif toX < x and not isVisited(visited, {
-            x = x - 1,
-            y = y
-        }) then
-            x = x - 1
-        elseif toY > y and not isVisited(visited, {
-            x = x,
-            y = y + 1
-        }) then
-            y = y + 1
-        elseif toY < y and not isVisited(visited, {
-            x = x - 1,
-            y = y
-        }) then
-            y = y - 1
-        else
-            newPath = false
-        end
-        if newPath then
-            table.insert(visited, {
-                x = x,
-                y = y
-            })
-            table.insert(path, {
-                x = x,
-                y = y
-            })
-        else
-            x = path[#path].x
-            y = path[#path].y
-            table.remove(path, #path)
-        end
-        if x == toX and y == toY then
-            break
-        end
-    end
-    return path
+    return galaxies, rankExpanded, fileExpanded
 end
 
 local function answer1(expand)
-    local input = getInput()
-    local galaxies = {}
+    local galaxies, rankExpanded, fileExpanded = getInput()
     local totalPath = 0
-    for y, rank in ipairs(input) do
-        for x, item in ipairs(rank) do
-            if item == "#" then
-                table.insert(galaxies, {
-                    x = x,
-                    y = y
-                })
-            end
-        end
-    end
     while #galaxies > 1 do
         for i = 2, #galaxies do
-            local path = getPath(galaxies[1], galaxies[i])
-            for _, val in ipairs(path) do
-                totalPath = totalPath + 1
-                if contains(input.fileExpanded, val.x) or contains(input.rankExpanded, val.y) then
+            totalPath = totalPath + math.abs(galaxies[1].x - galaxies[i].x) + math.abs(galaxies[1].y - galaxies[i].y)
+            for _, val in ipairs(fileExpanded) do
+                if (galaxies[i].x > val and galaxies[1].x < val) or (galaxies[i].x < val and galaxies[1].x > val) then
+                    totalPath = totalPath + expand
+                end
+            end
+            for _, val in ipairs(rankExpanded) do
+                if (galaxies[i].y > val and galaxies[1].y < val) or (galaxies[i].y > val and galaxies[1].y < val) then
                     totalPath = totalPath + expand
                 end
             end
@@ -128,6 +63,6 @@ local function answer1(expand)
     end
     return totalPath
 end
--- 40 seconds
+
 print("Parte 1:", answer1(1))
 print("Parte 2:", answer1(999999))
