1a44706f6e0f7dd7dd69088f1306b7d14558d620
deybis <damv2007@gmail.com>
2024-12-01 09:25:39 -0600

    pequeña mejora

--- 2024/01.lua
+++ 2024/01.lua
@@ -2,18 +2,19 @@ local aoc = require "lib.aoc"
 
 local input = aoc.input.getInput()
 input = aoc.string.split(input, "\n")
+local list1 = {}
+local list2 = {}
+
+for _, line in ipairs(input) do
+    line = aoc.string.split(line, "%s")
+    table.insert(list1, tonumber(line[1]))
+    table.insert(list2, tonumber(line[2]))
+end
 
 local function min(a, b) return a < b end
 
 local function answer1()
-    local list1 = {}
-    local list2 = {}
     local totalDistance = 0
-    for _, line in ipairs(input) do
-        line = aoc.string.split(line, "%s")
-        table.insert(list1, tonumber(line[1]))
-        table.insert(list2, tonumber(line[2]))
-    end
     table.sort(list1, min)
     table.sort(list2, min)
     for i, id in ipairs(list1) do
@@ -23,14 +24,7 @@ local function answer1()
 end
 
 local function answer2()
-    local list1 = {}
-    local list2 = {}
     local totalSimilarityScore = 0
-    for _, line in ipairs(input) do
-        line = aoc.string.split(line, "%s")
-        table.insert(list1, tonumber(line[1]))
-        table.insert(list2, tonumber(line[2]))
-    end
     for _, id in ipairs(list1) do
         for _, id2 in ipairs(list2) do
             if id == id2 then
@@ -42,4 +36,4 @@ local function answer2()
 end
 
 print("answer 1 is " .. answer1())
-print("answer 1 is " .. answer2())
+print("answer 2 is " .. answer2())
