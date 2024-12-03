a5952251248320b6b0765f5d420243ab73164c5d
deybis <damv2007@gmail.com>
2024-12-03 09:23:31 -0600

    ligera optimizacion

--- 2024/03.lua
+++ 2024/03.lua
@@ -1,22 +1,24 @@
 local aoc = require "lib.aoc"
 
 local input = aoc.input.getInput()
+local mulPattern = "mul%((%d+),(%d+)%)"
+local dontDoPattern = "don't%(%)(.-)do%(%)"
 
-local function answer1()
+local function getTotalMul(str)
     local total = 0
-    for n1, n2 in string.gmatch(input, "mul%((%d+),(%d+)%)") do
+    for n1, n2 in string.gmatch(str, mulPattern) do
         total = total + n1 * n2
     end
     return total
 end
 
+local function answer1()
+    return getTotalMul(input)
+end
+
 local function answer2()
-    local cleanedInput = string.gsub(input, "don't%(%)(.-)do%(%)", "")
-    local total = 0
-    for n1, n2 in string.gmatch(cleanedInput, "mul%((%d+),(%d+)%)") do
-        total = total + n1 * n2
-    end
-    return total
+    local cleanedInput = string.gsub(input, dontDoPattern, "")
+    return getTotalMul(cleanedInput)
 end
 
 print("answer 1 is " .. answer1())
