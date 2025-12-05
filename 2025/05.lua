26a9e45fd84a134d40fbd047cc9d86ca0bdc6ee3
DeybisMelendez <damv2007@gmail.com>
2025-12-05 07:03:01 -0600

    agregado dia 5 2025

--- 2025/05.lua
+++ 2025/05.lua
@@ -4,6 +4,30 @@ local input = {
     ranges = aoc.string.split(inputText:match("(.*)\n\n"), "\n"),
     ids = aoc.string.split(inputText:match(".*\n\n(.*)"), "\n"),
 }
+local memoID = {}
+
+local function simplifyRanges(ranges)
+    table.sort(ranges, function(a, b)
+        return a[1] < b[1]
+    end)
+
+    local result = {}
+    local current = ranges[1]
+
+    for i = 2, #ranges do
+        local r = ranges[i]
+
+        if r[1] <= current[2] + 1 then
+            current[2] = math.max(current[2], r[2])
+        else
+            table.insert(result, current)
+            current = r
+        end
+    end
+
+    table.insert(result, current)
+    return result
+end
 
 local function answer1()
     local total = 0
@@ -25,4 +49,32 @@ local function answer1()
     return total
 end
 
+local function answer2()
+    local total = 0
+    for _, range in ipairs(input.ranges) do
+        local parts = aoc.string.split(range, "-")
+        local low, high = tonumber(parts[1]), tonumber(parts[2])
+        local inserted = false
+        for _, memo in ipairs(memoID) do
+            if memo[1] >= low and memo[1] <= high then
+                memo[1] = low
+                inserted = true
+            end
+            if memo[2] >= low and memo[2] <= high then
+                memo[2] = high
+                inserted = true
+            end
+        end
+        if not inserted then
+            table.insert(memoID, {low, high})
+        end
+    end
+    memoID = simplifyRanges(memoID)
+    for _,range in ipairs(memoID) do
+        total = total + range[2] - range[1] + 1
+    end
+    return total
+end
+
 print("answer 1 is " .. answer1())
+print(string.format("Answer 2: %18.0f",answer2()))
